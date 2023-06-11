808d1e69f80cc5e7271bfa3d12dd1a021849f760
mrsekut <k.cloudspider@gmail.com>
2023-06-11 23:59:54 +0900

    feat: add packages

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+	plugins: [react()],
+	test: {
+		environment: "jsdom",
+		setupFiles: "./src/utils/testSetup.ts",
+		includeSource: ["src/**/*.{ts,tsx}"],
+	},
+});
